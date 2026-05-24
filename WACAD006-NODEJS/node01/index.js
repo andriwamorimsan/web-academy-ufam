9ef0a34a37ac0a52d24820c61c86a16115aab077
andriw <andriwamorimsan@gmail.com>
2026-05-24 12:02:00 -0400

    exercicio 01

---
+++ WACAD006-NODEJS/node01/index.js
@@ -0,0 +1,44 @@
+const http = require('http');
+const fs = require('fs');
+
+const diretorio = process.argv[2];
+
+if (!diretorio) {
+    console.log('Informe um diretório!');
+    console.log('Exemplo: node index.js teste');
+    process.exit(1);
+}
+
+const server = http.createServer((req, res) => {
+
+    fs.readdir(diretorio, (err, arquivos) => {
+
+        if (err) {
+            res.writeHead(500, {
+                'Content-Type': 'text/html; charset=utf-8'
+            });
+
+            res.end('<h1>Erro ao ler diretório</h1>');
+            return;
+        }
+
+        res.writeHead(200, {
+            'Content-Type': 'text/html; charset=utf-8'
+        });
+
+        let html = '<h1>Arquivos do diretório</h1>';
+        html += '<ul>';
+
+        arquivos.forEach((arquivo) => {
+            html += `<li>${arquivo}</li>`;
+        });
+
+        html += '</ul>';
+
+        res.end(html);
+    });
+});
+
+server.listen(3333, () => {
+    console.log('Servidor rodando em http://localhost:3333');
+});
