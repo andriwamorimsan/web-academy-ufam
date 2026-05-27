b8652e1fbe46549ecb078094cd5268f61526b5fe
andriw <andriwamorimsan@gmail.com>
2026-05-26 20:36:34 -0400

    install depedences

---
+++ WACAD009-EXPRESS/aula01/utils/getEnv.ts
@@ -0,0 +1,13 @@
+import {cleanEnv, port, str} from 'envalid';
+import dotenv from "dotenv";
+
+dotenv.config({ quiet: true})
+ 
+function getEnv(){
+    return cleanEnv(process.env, {
+        PORT: port({default: 5590}),
+    })
+}
+
+export default getEnv;
+
