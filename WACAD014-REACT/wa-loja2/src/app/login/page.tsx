74b26a9f139f9b16c4607edf86c38c3bb639211f
unknown <andriwamorimsan@gmail.com>
2026-08-03 02:50:44 -0400

    change event

--- WACAD014-REACT/wa-loja2/src/app/login/page.tsx
+++ WACAD014-REACT/wa-loja2/src/app/login/page.tsx
@@ -1,8 +1,29 @@
 'use client'
 
 import Link from 'next/link'
+import {ChangeEvent,SubmitEvent, useState} from "react";
+import {useRouter} from "next/navigation";
 
 export default function Login() {
+
+    const router = useRouter();
+
+     const [form, setForm] = useState({
+         email: "",
+         password: "",
+     })
+
+    const handleChange = ({target}: ChangeEvent<HTMLInputElement> ) => {
+         const {id, value} = target
+        setForm({...form, [id]: value})
+        console.log(form)
+    }
+    const handleSubmit =   ( event: SubmitEvent<HTMLFormElement>) => {
+         event.preventDefault()
+
+        router.push('/')
+    }
+
     return (
         <main>
             <div className='container-fluid d-flex min-vh-100'>
@@ -11,7 +32,7 @@ export default function Login() {
                         <h2>Bem vindo à WA Loja!</h2>
                     </div>{' '}
                     <div className='col-12 col-md-8 d-flex justify-content-center align-items-center'>
-                        <form>
+                        <form onSubmit={handleSubmit}>
                             <div className='mb-3'>
                                 <label htmlFor='email' className='form-label'>
                                     Email
@@ -20,7 +41,9 @@ export default function Login() {
                                     type='email'
                                     className='form-control form-control-lg'
                                     id='email'
+                                    value={form.email}
                                     aria-describedby='email'
+                                    onChange={handleChange}
                                     required
                                 />
                             </div>
@@ -32,6 +55,8 @@ export default function Login() {
                                     type='password'
                                     className='form-control form-control-lg'
                                     id='password'
+                                    value={form.password}
+                                    onChange={handleChange}
                                     required
                                 />
                             </div>
