be2f9323d68295c942b072cf039c61042c085756
Andriw <andriwamorimsan@gmail.com>
2026-08-15 15:42:40 -0400

    fix: lab2

--- WACAD017-INTEGRACAO/lab2/app.js
+++ WACAD017-INTEGRACAO/lab2/app.js
@@ -3,3 +3,9 @@ console.log("Exemplo Simples");
 var moment = require('moment');
 var date = moment().format('LL');
 console.log(date);
+
+
+// da pra vincular com
+//    defaults:
+//       run:
+//         working-directory: WACAD017-INTEGRACAO/lab2
