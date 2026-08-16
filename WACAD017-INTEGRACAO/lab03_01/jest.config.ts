af967eee8c703d08a6f45388f66dd1d14acc3608
Andriw <andriwamorimsan@gmail.com>
2026-08-15 22:02:57 -0400

    add: teste.yml

---
+++ WACAD017-INTEGRACAO/lab03_01/jest.config.ts
@@ -0,0 +1,16 @@
+export default {
+  preset: 'ts-jest',
+  testEnvironment: 'node',
+}
+
+
+//import type { Config } from "jest";
+//
+// const config: Config = {
+//   preset: "ts-jest",
+//   testEnvironment: "node",
+//   collectCoverage: true,
+//   coverageProvider: "v8",
+// };
+//
+// export default config;
